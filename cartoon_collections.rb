551043402f393d5a631832b8c7a82c11022df019
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 04:23:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
   array.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types.each) 
+  cheese_types.any? {|b| array.include?
 end
