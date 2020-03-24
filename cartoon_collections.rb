d2b0586f790ae54931e34a6aefff139d5e824537
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 04:07:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
   array.each.with_index(1) { |dwarf, index| puts "#{index} #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
 end
 
