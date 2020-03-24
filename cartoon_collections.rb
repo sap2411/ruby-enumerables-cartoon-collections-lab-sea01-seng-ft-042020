d0a4a7d35ccde45e0e6e8bf9909f5745d4f69e20
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-24 04:24:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.any? {|b| array.include?(b)}
+  cheese_types.any? {|b| array.include?(b), if true return b}
 end
